1bdd5ded553045d3a12d407e6fce921fd93d3e11
mysterystarfish <paulhsu1016@gmail.com>
2025-03-04 14:03:18 +0800

    feat: impl drag potions

--- Assets/Map/PotionButton/PotionButton.cs
+++ Assets/Map/PotionButton/PotionButton.cs
@@ -1,25 +1,40 @@
 using MessagePipe;
-using NodeChange;
 using UnityEngine;
-using UnityEngine.Serialization;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using VContainer;
 
 namespace Map.PotionButton
 {
-    public class PotionButton : MonoBehaviour
+    public class PotionButton : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
     {
         [SerializeField] private Potion type;
         [SerializeField] private int _leftAmount;
-        [SerializeField] private Button _button;
+        
         [Inject] private IPublisher<UsePotionRemoveObstacleRequest> _publisher;
+
+        private RectTransform _rectTransform;
+        private Vector3 _originalRectPosition;
         private void Awake()
         {
-            _button = GetComponent<Button>();
-            _button.onClick.AddListener(() =>
-            {
-                _publisher.Publish(new UsePotionRemoveObstacleRequest(type.potionName));
-            });
+            _rectTransform = GetComponent<RectTransform>();
+            Debug.Log(_originalRectPosition);
+        }
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            _originalRectPosition = _rectTransform.position;
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            _rectTransform.position = eventData.position;
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            _rectTransform.position = _originalRectPosition;
+            _publisher.Publish(new UsePotionRemoveObstacleRequest(type.potionName));
         }
     }
 }
